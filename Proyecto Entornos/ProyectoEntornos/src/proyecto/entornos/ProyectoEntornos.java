577fcb9615d383e65c11aa16e15bd66f19f7c130
jesuli89eu <102811861+jesuli89eu@users.noreply.github.com>
2022-04-13 19:16:18 +0200

    prueba 2.12

--- Proyecto Entornos/ProyectoEntornos/src/proyecto/entornos/ProyectoEntornos.java
+++ Proyecto Entornos/ProyectoEntornos/src/proyecto/entornos/ProyectoEntornos.java
@@ -2,9 +2,10 @@ package proyecto.entornos;
 
 import java.time.LocalDate;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 /**
  * 
- * @author Jesul
+ * @author grupoEntornos
  */
 public class ProyectoEntornos {
 
@@ -19,34 +20,45 @@ public class ProyectoEntornos {
         String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
             "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
         int[] dias = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int actualA, actualM, actualD, nacA, nacM, nacD, edad;
-        actualA = Integer.parseInt(LocalDate.now().toString().substring(0, 4));
-        actualM = Integer.parseInt(LocalDate.now().toString().substring(5, 7));
-        actualD = Integer.parseInt(LocalDate.now().toString().substring(8, 10));
-
-        System.out.println("Bienvenido");
-
+        int actualA, actualM, actualD, nacA, nacM=0, nacD=0, edad;
+        actualA = LocalDate.now().getYear();
+        actualM = LocalDate.now().getMonthValue();
+        actualD = LocalDate.now().getDayOfMonth();
+        
+        JOptionPane.showMessageDialog(null,"Bienvenido");
         do {
-            nacA = ingresarAnio();
+            
+            try{
+                nacA = ingresarAnio();
 
-            if (nacA > actualA) {
-                System.out.println("El año de nacimiento no puede ser posterior a la fecha de hoy.");
+                if (nacA > actualA) {
+                    JOptionPane.showMessageDialog(null,"El año de nacimiento no puede ser posterior a la fecha de hoy.");
+                }
+            }catch(NumberFormatException nfe){
+                JOptionPane.showMessageDialog(null,"Debes introducir sólo números enteros");
+                System.out.println("Excepción en el año: " + nfe);
+                nacA=actualA+1;
             }
-
+            
         } while (nacA > actualA);
 
         do {
             continua = false;
+            try{
+                nacM = ingresarMes();
 
-            nacM = ingresarMes();
+                if (nacM > 12 || nacM<1) {
+                    JOptionPane.showMessageDialog(null,"El mes debe ser un número entre 1 y 12.");
+                    continua = true;
+                } else if (nacA == actualA && nacM > actualM) {
 
-            if (nacM > 12) {
-                System.out.println("El mes debe ser un número entre 1 y 12.");
-                continua = true;
-            } else if (nacA == actualA && nacM > actualM) {
-
-                System.out.println("El mes no puede ser posterior al actual estando "
-                        + "en el mismo año.");
+                    JOptionPane.showMessageDialog(null,"El mes no puede ser posterior al actual estando "
+                            + "en el mismo año.");
+                    continua = true;
+                }
+            }catch(NumberFormatException nfe){
+                JOptionPane.showMessageDialog(null,"Debes introducir sólo números enteros");
+                System.out.println("Excepción en el mes: " + nfe);
                 continua = true;
             }
 
@@ -58,19 +70,24 @@ public class ProyectoEntornos {
 
         do {
             continua = false;
-            
+            try{
             nacD = ingresarDia();
 
-            if (nacD > dias[nacM - 1]) {
-                System.out.println("No existe ese día en " + meses[nacM - 1]);
+            if (nacD > dias[nacM - 1] || nacD<1) {
+                JOptionPane.showMessageDialog(null,"No existe ese día en " + meses[nacM - 1]);
                 continua = true;
             } else if (nacA == actualA && nacM == actualM && nacD > actualD) {
 
-                System.out.println("El día no puede ser posterior al actual estando "
+                JOptionPane.showMessageDialog(null,"El día no puede ser posterior al actual estando "
                         + "en el mismo mes y año.");
                 continua = true;
             }
-
+            }catch(NumberFormatException nfe){
+                JOptionPane.showMessageDialog(null,"Debes introducir sólo números enteros");
+                System.out.println("Excepción en el día: " + nfe);
+                continua = true;
+            }
+            
         } while (continua);
 
         edad = actualA - nacA;
@@ -87,17 +104,21 @@ public class ProyectoEntornos {
         if (edad == 1) {
             anio = "año completo";
         }
-
-        System.out.println("La edad es " + edad + " " + anio + ".");
+        
+        JOptionPane.showMessageDialog(null, "La edad es " + edad + " " + anio + ".");
+                
     }
     /**
      * 
-     * @return 
+     * @return
+     * @throws NumberFormatException 
      */
-    public static int ingresarAnio() {
+    public static int ingresarAnio() throws NumberFormatException {
         int anio;
-        System.out.println("Introduce el año de nacimiento.");
-        anio = sc.nextInt();
+        
+        anio  = Integer.parseInt(JOptionPane.showInputDialog("Introduce el año de nacimiento."));
+        //System.out.println("Introduce el año de nacimiento.");
+        //anio = sc.nextInt();
 
         return anio;
     }
@@ -105,10 +126,12 @@ public class ProyectoEntornos {
      * 
      * @return 
      */
-    public static int ingresarMes() {
+    public static int ingresarMes() throws NumberFormatException {
         int mes;
-        System.out.println("Introduce el mes de nacimiento.");
-        mes = sc.nextInt();
+        
+        mes  = Integer.parseInt(JOptionPane.showInputDialog("Introduce el mes de nacimiento."));
+        /*System.out.println("Introduce el mes de nacimiento.");
+        mes = sc.nextInt();*/
 
         return mes;
     }
@@ -116,10 +139,12 @@ public class ProyectoEntornos {
      * 
      * @return el dia como int
      */
-    public static int ingresarDia() {
+    public static int ingresarDia() throws NumberFormatException {
         int dia;
-        System.out.println("Introduce el día de nacimiento.");
-        dia = sc.nextInt();
+        
+        dia = Integer.parseInt(JOptionPane.showInputDialog("Introduce el día de nacimiento."));
+        /*System.out.println("Introduce el día de nacimiento.");
+        dia = sc.nextInt();*/
 
         return dia;
     }
